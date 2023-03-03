bc352ee06de5445d14304250dcbf5f0913f0bdd1
j-j-brinkmann <j.j.brinkmann@web.de>
2023-03-03 14:01:43 +0100

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi
+        voluptate vel quos veniam libero explicabo, eligendi culpa reprehenderit
+        cupiditate perspiciatis vitae nesciunt possimus, expedita atque a? Ipsa
+        eaque nesciunt excepturi placeat id ad animi! Libero hic optio officiis
+        temporibus voluptatem?
+      </p>
+    </section>
+  );
+};
+
+export default About;
